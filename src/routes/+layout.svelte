f5b2cf2e10244ff60c3ca07547aea205467e0e32
Elliot Voris <elliot@stellar.org>
2025-02-06 12:31:55 -0600

    use vercel adapter

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,16 +7,13 @@
     import PageContent from '$lib/components/ui/PageContent.svelte';
     import Footer from '$lib/components/ui/Footer.svelte';
 
-    import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
     import {
         initializeStores,
-        storePopup,
         Modal,
         Toast,
         autoModeWatcher,
     } from '@skeletonlabs/skeleton';
     import type { LayoutProps } from './$types';
-    storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
     initializeStores();
 
     let { children }: LayoutProps = $props();
