958c31947c33624421722a9378d80810cdeeee06
Elliot Voris <elliot@stellar.org>
2025-03-26 11:03:36 -0500

    ts ignore some things

--- src/lib/contracts/trading_post.ts
+++ src/lib/contracts/trading_post.ts
@@ -2,6 +2,7 @@ import * as Client from 'trading_post';
 import { PUBLIC_STELLAR_RPC_URL, PUBLIC_STELLAR_NETWORK } from '$env/static/public';
 
 export default new Client.Client({
+    //@ts-ignore
     ...Client.networks[PUBLIC_STELLAR_NETWORK],
     rpcUrl: PUBLIC_STELLAR_RPC_URL,
 });
