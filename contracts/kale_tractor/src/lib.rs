25cc2f190a60ee7afec65e42d5e083033fed9f25
Elliot Voris <elliot@stellar.org>
2025-03-28 11:44:12 -0500

    change tractor contract to use `try_harvest` instead of failing
    add much better tests, too!

--- contracts/kale_tractor/src/lib.rs
+++ contracts/kale_tractor/src/lib.rs
@@ -1,7 +1,7 @@
 #![no_std]
 
 use soroban_sdk::{
-    contract, contractclient, contractimpl, contractmeta, symbol_short, Address, Env, Symbol, Vec,
+    contract, contractclient, contracterror, contractimpl, contractmeta, panic_with_error, symbol_short, vec, Address, Env, Symbol, Vec
 };
 
 contractmeta!(key = "title", val = "KaleFail Tractor",);
@@ -14,6 +14,16 @@ contractmeta!(key = "binver", val = "1.0.0",);
 const FARM: Symbol = symbol_short!("FARM");
 const DAY_OF_LEDGERS: u32 = 60 * 60 * 24 / 5; // assumes 5-second ledger close times
 const WEEK_OF_LEDGERS: u32 = DAY_OF_LEDGERS * 7;
+
+#[contracterror]
+#[derive(Copy, Clone, Debug, Eq, PartialEq, PartialOrd, Ord)]
+#[repr(u32)]
+pub enum Error {
+    /// No pails provided in invocation
+    NoPailsProvided = 1,
+    /// Harvesting all pails results in 0 reward
+    NoHarvestablePails = 2,
+}
 
 #[contractclient(name = "FarmClient")]
 pub trait Farm {
@@ -43,19 +53,33 @@ impl KaleTractorContract {
     ///
     /// - `farmer` - address of the farmer to harvest on behalf of
     /// - `pails` - vector of pails which should be harvested
-    pub fn harvest(env: Env, farmer: Address, pails: Vec<u32>) -> i128 {
-        let mut total_reward: i128 = 0;
+    pub fn harvest(env: Env, farmer: Address, pails: Vec<u32>) -> Vec<i128> {
+        if pails.len() == 0 {
+            panic_with_error!(&env, Error::NoPailsProvided);
+        }
+
+        let mut rewards: Vec<i128> = vec![&env];
         let farm_address: Address = env.storage().instance().get(&FARM).unwrap();
         let farm_client = FarmClient::new(&env, &farm_address);
 
+        // iterate through pails provided
         pails.iter().for_each(|pail| {
-            let reward = farm_client.harvest(&farmer, &pail);
-            total_reward += reward;
+            // try to invoke the farm's `harvest` function
+            let reward = match farm_client.try_harvest(&farmer, &pail) {
+                Ok(Ok(number)) => number, // successful harvest, use the reward
+                _ => 0, // unsuccessful for some reason, use 0
+            };
+            rewards.push_back(reward);
         });
+
+        // make sure we've at least harvested _some_ kale
+        if rewards.iter().sum::<i128>() == 0 {
+            panic_with_error!(&env, Error::NoHarvestablePails);
+        };
 
         extend_instance_ttl(&env);
 
-        return total_reward;
+        return rewards;
     }
 }
 
--- contracts/kale_tractor/src/test.rs
+++ contracts/kale_tractor/src/test.rs
@@ -2,8 +2,9 @@
 
 use super::*;
 use soroban_sdk::{
-    testutils::Address as _, Env,
+    testutils::{Address as _, Ledger}, Env,
 };
+extern crate std;
 
 mod homestead_contract {
     use soroban_sdk::auth::Context;
@@ -21,4 +22,77 @@ fn test_constructor() {
     let homestead_address = env.register(homestead_contract::WASM, (admin, kale.address()));
 
     env.register(KaleTractorContract, (homestead_address,));
+}
+
+#[test]
+#[should_panic(expected = "Error(Contract, #1)")]
+fn test_with_no_pails_provided() {
+    let env = Env::default();
+    env.mock_all_auths_allowing_non_root_auth();
+
+    let admin = Address::generate(&env);
+    let farmer = Address::generate(&env);
+
+    let kale = env.register_stellar_asset_contract_v2(admin.clone());
+    let homestead_address = env.register(homestead_contract::WASM, (admin, kale.address()));
+
+    let tractor_address = env.register(KaleTractorContract, (homestead_address,));
+    let tractor_client = KaleTractorContractClient::new(&env, &tractor_address);
+    tractor_client.harvest(&farmer, &vec![&env]);
+}
+
+#[test]
+fn test_with_all_harvestable_pails() {
+    let env = Env::from_ledger_snapshot_file("snapshot.json");
+    env.ledger().set_max_entry_ttl(56000000);
+
+    let homestead_address = Address::from_str(&env, &"CDL74RF5BLYR2YBLCCI7F5FB6TPSCLKEJUBSD2RSVWZ4YHF3VMFAIGWA");
+
+    let tractor_address = env.register(KaleTractorContract, (homestead_address,));
+    let tractor_client = KaleTractorContractClient::new(&env, &tractor_address);
+
+    // these pails, for this user, just so happen to be present and harvestable given the snapshot file.
+    let farmer = Address::from_str(&env, &"GBQZZ54DGBXZ6USGU5PMMAJVDLALGEMXXZEZC5BDDSAG7FI653DLMVTN");
+    let rewards = tractor_client.harvest(&farmer, &vec![&env, 11692, 11697, 11698, 11699, 11700, 11701]);
+
+    assert!(!rewards.is_empty());
+    assert!(rewards.iter().all(|r| r > 0));
+    assert_eq!(rewards.iter().sum::<i128>(), 21566344);
+    assert_eq!(rewards.len(), 6);
+}
+
+#[test]
+#[should_panic(expected = "Error(Contract, #2)")]
+fn test_with_all_unharvestable_pails() {
+    let env = Env::default();
+    env.mock_all_auths_allowing_non_root_auth();
+
+    let admin = Address::generate(&env);
+    let farmer = Address::generate(&env);
+
+    let kale = env.register_stellar_asset_contract_v2(admin.clone());
+    let homestead_address = env.register(homestead_contract::WASM, (admin, kale.address()));
+
+    let tractor_address = env.register(KaleTractorContract, (homestead_address,));
+    let tractor_client = KaleTractorContractClient::new(&env, &tractor_address);
+    tractor_client.harvest(&farmer, &vec![&env, 0, 1, 2, 3]);
+}
+
+#[test]
+fn test_with_mixed_harvestable_pails() {
+    let env = Env::from_ledger_snapshot_file("snapshot.json");
+    env.ledger().set_max_entry_ttl(56000000);
+
+    let homestead_address = Address::from_str(&env, &"CDL74RF5BLYR2YBLCCI7F5FB6TPSCLKEJUBSD2RSVWZ4YHF3VMFAIGWA");
+
+    let tractor_address = env.register(KaleTractorContract, (homestead_address,));
+    let tractor_client = KaleTractorContractClient::new(&env, &tractor_address);
+
+    let farmer = Address::from_str(&env, &"GBQZZ54DGBXZ6USGU5PMMAJVDLALGEMXXZEZC5BDDSAG7FI653DLMVTN");
+    let rewards = tractor_client.harvest(&farmer, &vec![&env, 0, 1, 2, 3, 11692, 11697, 11698, 11699, 11700, 11701]);
+
+    assert!(!rewards.is_empty());
+    assert!(rewards.iter().all(|r| r >= 0));
+    assert_eq!(rewards.iter().sum::<i128>(), 21566344);
+    assert_eq!(rewards.len(), 10);
 }
