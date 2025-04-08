304989db9870b0533a1f7eb96db8db891e984a8f
Elliot Voris <elliot@stellar.org>
2025-04-08 11:37:29 -0500

    add a notification bar to prompt users to upgrade

---
+++ src/lib/components/ui/UpgradeWalletBar.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import { slide } from 'svelte/transition';
+    import { wallet } from '$lib/state/Wallet.svelte';
+</script>
+
+{#if wallet.upgradeAvailable}
+    <div transition:slide class="flex bg-error-100-800-token p-2">
+        <div class="flex gap-2 items-center mx-auto">
+            <div>Wallet upgrade available</div>
+            <a href="/upgrade" class="btn btn-sm variant-filled-secondary">Upgrade Now</a>
+        </div>
+    </div>
+{/if}
