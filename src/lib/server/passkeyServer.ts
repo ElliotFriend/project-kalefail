26fbff8a5652f74a8a524831b43a7ae000c21e45
Elliot Voris <elliot@stellar.org>
2025-04-18 20:59:29 -0500

    add timeout and lt headers

--- src/lib/server/passkeyServer.ts
+++ src/lib/server/passkeyServer.ts
@@ -6,11 +6,16 @@ import {
     PUBLIC_STELLAR_RPC_URL,
 } from '$env/static/public';
 import { PRIVATE_LAUNCHTUBE_JWT, PRIVATE_MERCURY_TOKEN } from '$env/static/private';
+import { version } from '../../../package.json'
 
 export const server = new PasskeyServer({
     rpcUrl: PUBLIC_STELLAR_RPC_URL,
     launchtubeUrl: PUBLIC_LAUNCHTUBE_URL,
     launchtubeJwt: PRIVATE_LAUNCHTUBE_JWT,
+    launchtubeHeaders: {
+        'X-Client-Name': 'kalefail-website',
+        'X-Client-Version': version,
+    },
     mercuryProjectName: 'smart-wallets-kalefail',
     mercuryUrl: PUBLIC_MERCURY_URL,
     mercuryKey: PRIVATE_MERCURY_TOKEN,
